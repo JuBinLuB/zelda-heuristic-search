bcac9a7f3ea436c6e52d371f32554bc010e4a452
Alexssander Fernandes <alexssander84@hotmail.com>
2025-08-14 16:59:15 -0300

    modified:   src/build_map.py 	modified:   src/main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-from build_map import build_map
+from build_map import build_map_from_txt
 
 def main():
-    build_map(f"../Datasets/txt/dungeon_2.txt", f"../Datasets/bmp/dungeon_2.bmp")
+    build_map_from_txt(f"../Datasets/txt/dungeon_2.txt", f"../Datasets/bmp/dungeon_2.bmp")
 
 if __name__ == "__main__":
     main()
