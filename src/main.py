7bc8a3c86a01af13145f9f195a147d40766a8c0d
Alexssander Fernandes <alexssander84@hotmail.com>
2025-08-14 16:33:42 -0300

    modified:   src/main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from build_map import build_map
 
 def main():
-    build_map("../Datasets/txt/main_map.txt", "../Datasets/bmp/main_map.bmp")
+    build_map(f"../Datasets/txt/dungeon_2.txt", f"../Datasets/bmp/dungeon_2.bmp")
 
 if __name__ == "__main__":
     main()
